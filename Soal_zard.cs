b8182e693587001d20e28d8ff1136722dcfd0852
Mahdi_seyedi <104705171+Mahdiseyedi@users.noreply.github.com>
2022-07-06 21:45:32 +0430

    Add files via upload

---
+++ Soal_zard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soal_zard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            string mid_word = "";
+            for (int i = 0; i < num; i++)
+            {
+                mid_word += "o";
+            }
+            Console.WriteLine("W" + mid_word + "w!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tafazol_game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tafazol_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string init_num = Console.ReadLine();
+
+            int game(string num)
+            {
+                var num_1 = Convert.ToInt32(num[0]);
+                var num_2 = Convert.ToInt32(num[1]);
+                if (num_1 >= num_2)
+                {
+                    return (num_1 - num_2);
+                }
+                else
+                {
+                    return (num_2 - num_1);
+                }
+            }
+
+            Console.WriteLine(game(init_num));
+
+            Console.ReadKey();
+        }
+    }
+}
