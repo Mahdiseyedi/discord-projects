d127bd45cfc2df4682656f99e223f90e58181a13
Mahdi_seyedi <104705171+Mahdiseyedi@users.noreply.github.com>
2022-07-07 15:30:32 +0430

    Add files via upload

---
+++ Barare.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barare_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            if ( k % 2 == 0)
+            {
+                Console.WriteLine("Bala Barare");
+            }
+            else
+            {
+                Console.WriteLine("Payin Barare");
+            }
+            Console.ReadKey();
+        }
+    }
+}
