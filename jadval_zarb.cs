beadff3db93e3164313e48e04839d94e5865ec0b
Mahdi_seyedi <104705171+Mahdiseyedi@users.noreply.github.com>
2022-07-07 15:18:39 +0430

    Add files via upload

---
+++ jadval_zarb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jadval_zarb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= num; j++)
+                {
+                    Console.Write(i*j);
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
